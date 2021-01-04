e38fb91e8b9eb4b5027b6bffb158e96430b252cd
raam54 <raam54@icloud.com>
2021-01-04 22:51:25 +0300

    added final distribution

--- Shift_Distribution/ViewController.swift
+++ Shift_Distribution/ViewController.swift
@@ -7,33 +7,34 @@
 
 import UIKit
 
-extension UIColor {
-    public convenience init?(hex: String) {
-        let r, g, b, a: CGFloat
 
-        if hex.hasPrefix("#") {
-            let start = hex.index(hex.startIndex, offsetBy: 1)
-            let hexColor = String(hex[start...])
-
-            if hexColor.count == 8 {
-                let scanner = Scanner(string: hexColor)
-                var hexNumber: UInt64 = 0
-
-                if scanner.scanHexInt64(&hexNumber) {
-                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
-                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
-                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
-                    a = CGFloat(hexNumber & 0x000000ff) / 255
-
-                    self.init(red: r, green: g, blue: b, alpha: a)
-                    return
-                }
-            }
-        }
-
-        return nil
-    }
-}
+//extension UIColor {
+//    public convenience init?(hex: String) {
+//        let r, g, b, a: CGFloat
+//
+//        if hex.hasPrefix("#") {
+//            let start = hex.index(hex.startIndex, offsetBy: 1)
+//            let hexColor = String(hex[start...])
+//
+//            if hexColor.count == 8 {
+//                let scanner = Scanner(string: hexColor)
+//                var hexNumber: UInt64 = 0
+//
+//                if scanner.scanHexInt64(&hexNumber) {
+//                    r = CGFloat((hexNumber & 0xff000000) >> 24) / 255
+//                    g = CGFloat((hexNumber & 0x00ff0000) >> 16) / 255
+//                    b = CGFloat((hexNumber & 0x0000ff00) >> 8) / 255
+//                    a = CGFloat(hexNumber & 0x000000ff) / 255
+//
+//                    self.init(red: r, green: g, blue: b, alpha: a)
+//                    return
+//                }
+//            }
+//        }
+//
+//        return nil
+//    }
+//}
 
 class ViewController: UIViewController {
     var people: [String] = ["Кириллова", "Ленева", "Соколова", "Солодовник", "Сергушева", "Осипова", "Ширабайкина"]
@@ -67,8 +68,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.setupLabelTap()
+        
         cellArr = [FirstLbl, SecondLbl, ThirdLbl, FourthLbl, FifthLbl, SixthLbl, SeventhLbl, EightsLbl, NinthLbl, TenthLbl, EleventhLbl]
+
+        
         for subview in self.view.subviews as! [UIView] {
 
             if let label = subview as? UILabel {
@@ -80,7 +83,7 @@ class ViewController: UIViewController {
         }
         view.sendSubviewToBack(designView)
         designView.transform = CGAffineTransform(rotationAngle: 45)
-        designView.backgroundColor = UIColor(hex: "#c0c0c0ff")
+        designView.backgroundColor = .lightGray
         title = "Shift Distribution"
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addPersonAC))
@@ -100,112 +103,105 @@ class ViewController: UIViewController {
     }
 
     @objc func distribute() {
-        var tmpPeople1 = people
-        //tmpPeople1.shuffle()
-        var tmpPeople2 = tmpPeople1
-        //tmpPeople2.shuffle()
-        tmpPeople2 = tmpPeople2.reversed()
-        
-//        for i in 0..<tmpPeople1.count {
-//            if tmpPeople1[i] == tmpPeople2[i] { tmpPeople1.shuffle() }
-//        }
-        
-        
+        var tmpPeople = people
+          
         FirstLbl.text = "Рыжова"
-        FirstLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        FirstLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        FirstLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
+        FirstLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FirstLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FirstLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FirstLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FirstLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
         
-        SecondLbl.text = tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        tmpPeople1.remove(at: 0)
-        SecondLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        tmpPeople1.remove(at: 0)
-        
-        tmpPeople1 = people.shuffled()
-        tmpPeople2 = tmpPeople1
-        tmpPeople2 = tmpPeople2.reversed()
-        
-        ThirdLbl.text = tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        ThirdLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        ThirdLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        ThirdLbl.text? += "\n" + tmpPeople1[0]
-        //tmpPeople1.remove(at: 0)
-        tmpPeople2.remove(at: 0)
-        
+
+        SecondLbl.text = tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SecondLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+
+        tmpPeople = people.shuffled()
+
+        ThirdLbl.text = tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        ThirdLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        ThirdLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        ThirdLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        ThirdLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        ThirdLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+
         FourthLbl.text = "Рыжова"
-        FourthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
+        FourthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
         
-        
+        tmpPeople = people.shuffled()
+
         FifthLbl.text = "Рыжова"
-        FifthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        FifthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        FifthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        //tmpPeople2.remove(at: 0)
+        FifthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FifthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FifthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FifthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        FifthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
         
-        SixthLbl.text = tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        SixthLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-       
-        tmpPeople1 = people.shuffled()
-        tmpPeople2 = tmpPeople1
-        tmpPeople2 = tmpPeople2.reversed()
-        
-        SeventhLbl.text = tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        SeventhLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        SeventhLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        SeventhLbl.text? += "\n" + tmpPeople1[0]
-       //tmpPeople1.remove(at: 0)
-        tmpPeople2.remove(at: 0)
-        
+        SixthLbl.text = tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SixthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+
+        tmpPeople = people.shuffled()
+
+        SeventhLbl.text? = tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SeventhLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SeventhLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SeventhLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SeventhLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        SeventhLbl.text? += "\n" + tmpPeople[0]
+
         EightsLbl.text = "Рыжова"
-        EightsLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
+        EightsLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
         
+        tmpPeople = people.shuffled()
+
         NinthLbl.text = "Рыжова"
-        NinthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        NinthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        NinthLbl.text? += "\n" + tmpPeople1[0]
-        tmpPeople1.remove(at: 0)
-        //tmpPeople2.remove(at: 0)
-        
-        TenthLbl.text = tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        TenthLbl.text? += "\n" + tmpPeople2[0]
-        tmpPeople2.remove(at: 0)
-        
+        NinthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        NinthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        NinthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        NinthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        NinthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+
+        TenthLbl.text = tmpPeople[0]
+        tmpPeople.remove(at: 0)
+        TenthLbl.text? += "\n" + tmpPeople[0]
+        tmpPeople.remove(at: 0)
+
         people.shuffle()
         EleventhLbl.text = people[0]
     }
     
-    @objc func labelTapped(_ sender: UITapGestureRecognizer) {
-            print("labelTapped")
-        }
-        
-    func setupLabelTap() {
-            
-            let labelTap = UITapGestureRecognizer(target: self, action: #selector(self.labelTapped(_:)))
-        self.view.label.isUserInteractionEnabled = true
-        self.view.label.addGestureRecognizer(labelTap)
-            
-        }
     
 }
 
