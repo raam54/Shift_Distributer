06d0b7459a80216bffd83d75a3ce3c10f7c21565
Nick <raam54@icloud.com>
2021-07-16 13:00:36 +0300

    added UserDefaults for storing cell data

---
+++ Cells+Types/CodableExtension+Image.swift
@@ -0,0 +1,8 @@
+//
+//  CodableExtension+Image.swift
+//  Shift_Distribution
+//
+//  Created by Nick on 15.07.2021.
+//
+
+import Foundation
---
+++ Cells+Types/CodableExtension.swift
@@ -0,0 +1,8 @@
+//
+//  CodableExtension.swift
+//  Shift_Distribution
+//
+//  Created by Nick on 15.07.2021.
+//
+
+import Foundation
